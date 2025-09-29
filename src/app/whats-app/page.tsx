21b7f04195f990ba0c7942c225d96686f13001cd
macos <nikhil.tekawade@evonix.co>
2025-09-29 17:20:35 +0530

    Nikhil: Ritika add new page in whats-app folder for sucess show

---
+++ src/app/whats-app/page.tsx
@@ -0,0 +1,18 @@
+// success page after form submission
+
+'use client'
+
+const SuccessFormPage: React.FC = () => {
+
+
+  return (
+    <div className='apply-task-container'>
+        <div className="form-group">
+          <b>Form submitted successfully!</b>
+        </div>
+    </div>
+  )
+}
+
+export default SuccessFormPage
+
